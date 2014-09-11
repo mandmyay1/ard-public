7380b3c40c80282137c98a9de8bd96f4eea2c397
mandmyay1 <adam.rhoda@gmail.com>
2014-09-10 21:55:34 -0700

    Update greyscale slightly
    Update pixels in place rather than creating a new Image

--- Greyscale/greyscale/__init__.py
+++ Greyscale/greyscale/__init__.py
@@ -5,12 +5,10 @@ file = input("Enter file to conver to greyscale ")
 newFile = os.path.join( os.path.dirname(file), "grey-" + os.path.basename(file) )
 colorImg = Image.open(file)
 colorPixels = colorImg.load()
-greyImg = Image.new("RGB",colorImg.size,"white")
-greyPixels = greyImg.load()
 for i in range(colorImg.size[0]):
     for j in range(colorImg.size[1]):
         pixel = colorPixels[i,j]
         luminosityPixel = int(0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2])
-        greyPixels[i,j] = (luminosityPixel,luminosityPixel,luminosityPixel)
-greyImg.save(newFile)
+        colorPixels[i,j] = (luminosityPixel,luminosityPixel,luminosityPixel)
+colorImg.save(newFile)
 print("Saved greyscale copy at", newFile)
