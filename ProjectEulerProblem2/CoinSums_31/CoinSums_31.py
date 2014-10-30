3a76623dc44e5e40b0087f3b7a0eb6cc84c874a2
mandmyay1 <adam.rhoda@gmail.com>
2014-10-29 20:18:25 -0700

    Coin Sums 31

--- ProjectEulerProblem2/CoinSums_31/CoinSums_31.py
+++ ProjectEulerProblem2/CoinSums_31/CoinSums_31.py
@@ -1,28 +1,12 @@
-from itertools import product
 
 listAvailable = [1,2,5,10,20,50,100,200]
-def numberCoins(total):
-    combinations = 0
-    for num in listAvailable:
-        if total == num: combinations += 1
-        elif total - num < 0: pass
-        else: combinations += numberCoins( total - num )
-    return combinations
-
-print(numberCoins(3))
-
-for i in range(len(listAvailable)):
-    coin = listAvailable[i]
-    s = [coin]
-    while sum(s) < 200:
-        for j in range(i,len(listAvailable)):
-            pass
-    
-"""
-setCombs = set()
-for i in range(1,201):
-    currentCombs = list(filter(lambda x: sum(x) == 200, product(listAvailable, repeat=i)))
-    for comb in currentCombs:
-        setCombs.add(tuple(sorted(list(comb))))
-    
-print(len(setCombs))"""
+def changes(amount, coins):
+    ways = [0] * (amount + 1)
+    ways[0] = 1
+    for coin in coins:
+        for j in range(coin, amount + 1):
+            ways[j] += ways[j - coin]
+    return ways[amount]
+ 
+print (changes(200, listAvailable))
+#print changes(100000, [1, 5, 10, 25, 50, 100])
